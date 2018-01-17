5c27b719d7ac55a7e7a795995623c9f322fbe1db
Vadim.Isaev <vadim.o.isaev@gmail.com>
2018-01-17 09:53:43 +0600

    cache

--- src/main/java/tech/lapsa/javax/jpa/commons/JPAConstants.java
+++ src/main/java/tech/lapsa/javax/jpa/commons/JPAConstants.java
@@ -1,5 +1,12 @@
 package tech.lapsa.javax.jpa.commons;
 
+import java.util.Map;
+
+import javax.persistence.CacheRetrieveMode;
+import javax.persistence.CacheStoreMode;
+
+import tech.lapsa.java.commons.function.MyMaps;
+
 public final class JPAConstants {
 
     private JPAConstants() {
@@ -7,4 +14,9 @@ public final class JPAConstants {
 
     public static final String HINT_JAVAX_PERSISTENCE_CACHE_STORE_MODE = "javax.persistence.cache.storeMode";
     public static final String HINT_JAVAX_PERSISTENCE_CACHE_RETREIVE_MODE = "javax.persistence.cache.retreiveMode";
+
+    public static final Map<String, Object> NO_CACHE_PROPERTIES = MyMaps.of( //
+	    HINT_JAVAX_PERSISTENCE_CACHE_RETREIVE_MODE, CacheRetrieveMode.BYPASS, //
+	    HINT_JAVAX_PERSISTENCE_CACHE_STORE_MODE, CacheStoreMode.REFRESH);
+
 }
